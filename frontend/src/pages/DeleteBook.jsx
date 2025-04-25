8d517449c2433e76a0b671002076da3a97f31660
Sooraj GV <soorajgvaidya@gmail.com>
2025-04-25 11:17:05 +0530

    Update DeleteBook.jsx

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -11,7 +11,7 @@ const DeleteBook = () => {
 
   const handleDeleteBook =(()=>{
   setLoading(true);
-  axios.delete(`http://localhost:5555/books/${id}`)
+  axios.delete(`https://backend-service.onrender.com/books/${id}`)
   .then(()=>{
     setLoading(false);
     navigate('/');
